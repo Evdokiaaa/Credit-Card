3cc708dc63d132bb92f986e1ad34a1daaa95deb9
Evdokia <vladalt1231@gmail.com>
2023-09-23 19:41:35 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const inputs = document.querySelectorAll("input");
+//front card
+
+//name
+const frontCardName = document.querySelector(".card__front-name");
+const frontCardNameInput = document.querySelector("#cardHolder");
+const frontCardNameError = document.querySelector(".form__error-name");
+//numbers
+const frontCardNumbers = document.querySelector(".card__front-number");
+const frontCardNumbersInput = document.querySelector("#cardNumber");
+const frontCardNumbersError = document.querySelector(".form__error-number");
+//month
+const frontCardMonth = document.querySelector(".card__front-month");
+const frontCardMonthInput = document.querySelector("#expirationMonth");
+const frontCardMonthError = document.querySelector();
+//year
+const frontCardYear = document.querySelector(".card__front-year");
+const frontCardYearInput = document.querySelector("#expirationYear");
+const frontCardYearError = document.querySelector;
+//back card
+const backCard = document.querySelector(".card__front-month");
+
+let validateName = false;
+let validateNumber = false;
+let validateMonth = false;
+let validateYear = false;
+let validateCvc = false;
